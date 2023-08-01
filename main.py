02c786af911cd563b37b95c8d4d17a057e0d2c75
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-08-02 00:35:08 +0200

    refactor the entire code

--- main.py
+++ main.py
@@ -1,48 +1,62 @@
-# excuse my messy code, I'll clean it..
-
 import random
 
-randlist = [0,1,2,3,4,5,6,7,8,9] 
-c= 9
-randomnum = 0
-for i in range(4):
-  randtemp = random.randrange(0,c) 
-  temp = randlist[randtemp] 
-  randomnum += temp*(10**i) 
-  del randlist[randtemp] used again
-  c-=1 
 
-print("                game rules")
-print("cow mean it's right and in the right place")
-print('')
-print("key mean it's right but not in the right piace")
-print('')
-print("bull mean it's a wrong number")
-print('')
-print("write 4 numbers to guess the right number")
-print('')
-n=input()
-t=True
-while t:
-  if(n==randomnum):
-    print("We got a winner here!!")
-    t=False
-  else:
-    c=0
-    b=0
-    k=0
-    for i in range(randomnum):
-      if(n[i]==randomnum[i]):
-        c+=1 
-      elif(n[0]==randomnum[i] or n[1]==randomnum[i] or n[2]==randomnum[i] or n[3]==randomnum[i]):
-        k+=1
-      else: 
-        b+=1 
-    print("cows:",c)
-    print('')
-    print("keys:",k)
-    print('')
-    print("bulls:",b)
-    print('')
-    print("try again")
-    n=input() 
+def main():
+    random_num = generat_random_number()
+
+    print(
+        """
+                            game rules
+          - cow mean it's right and in the right place
+          - key means it's right but not in the right place
+          - bull means it's a wrong number
+          - write 4 numbers to guess the right number
+          """
+    )
+
+    playing(random_num)
+
+
+def generat_random_number():
+    numbers_range = "0123456789"
+    current_number = []
+
+    for _ in range(4):
+        random_char = random.choice(numbers_range)
+        current_number.append(random_char)
+        numbers_range = numbers_range.replace(random_char, "")
+
+    return current_number
+
+
+def playing(random_num):
+    inputed_number = input()
+    inputed_number_seq = [i for i in inputed_number]
+
+    if inputed_number_seq == random_num:
+        print("We got a winner here!!")
+        return
+    else:
+        cows = 0
+        bulls = 0
+        keys = 0
+
+        for i in range(4):
+            if inputed_number_seq[i] == random_num[i]:
+                cows += 1
+            elif inputed_number_seq[i] in random_num:
+                keys += 1
+            else:
+                bulls += 1
+        print(
+            f"""
+              cows: {cows} | keys: {keys} | bulls: {bulls}
+
+              try again
+              """
+        )
+        playing(random_num)
+
+
+if __name__ == "__main__":
+    main()
